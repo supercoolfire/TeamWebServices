a8cbc57893d08cf452c56cafbc95814261be8a0b
EMERY RODZIEWICZ <rod20012@byui.edu>
2023-11-28 20:58:34 -0500

    updated validation

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const usersController = require('../controllers/user');
 
+const validation = require('../middleware/validate');
 
 router.get('/', usersController.getAllUsers);
 router.get('/:id', usersController.getSingleUser);
-router.post('/', usersController.createUser);
-router.put('/:id', usersController.updateUser);
+
+router.post('/', validation.validateUser, usersController.createUser);
+router.put('/:id', validation.validateUser, usersController.updateUser);
+
 router.delete('/:id', usersController.deleteUser);
 
 
