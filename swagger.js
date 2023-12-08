204bf46a24c1796f584e1daa2f4576316c9f61c5
supercoolfire <u285916669@byui.edu>
2023-12-08 12:07:50 +0800

    not important update but essential

--- swagger.js
+++ swagger.js
@@ -1,21 +1,17 @@
-const swaggerAutogen = require('swagger-autogen');
-
-
-
-const host = 'localhost:3000';
-// const host = 'localhost:3001';
-// const host = 'https://project2-cqkb.onrender.com';
+const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
     info: {
         title: 'Designer Music'
     },
-    host: host,
-    basePath: '/',
-    schemes: ['http', 'https']
+    // host: 'localhost:3000',
+    // schemes: ['http', 'https']
+    host: 'cse341-node-project2-w4.onrender.com',
+    schemes: ['https', 'http']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
+// this wil generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
