5b5a4309ccc9ec601e55491b6bb0b0134124f53c
supercoolfire <u285916669@byui.edu>
2023-12-13 01:31:06 +0800

    fix error cause by browser autocomplete

--- routes/override.js
+++ routes/override.js
@@ -3,40 +3,36 @@ const express = require('express');
 
 router.use((req, res, next) => {
     const queryParameter = req.query.role;
+    roles = ['god', 'admin', 'moderator', 'default'];
+    currentUser = req.session.user ? req.session.user.username !== undefined ? req.session.user.username : '' : '';
     if (queryParameter) {
+        
         // console.log('override req.session.user:', JSON.stringify(req.session.user, null, 2));
-        if (req.query.role === 'god' && req.session.user === undefined) {
+        if (req.query.role === 'god' && (req.session.user === undefined || roles.includes(currentUser))) {
             console.log('switching to god mode')
             req.session.user = {}
             req.session.user = { username: 'Thor', displayName: 'Thor' }
             res.locals.message = `Logged in as ${req.session.user.username}`;
-            // return res.redirect(req.originalUrl);
-        } else if (req.query.role === 'admin' && req.session.user === undefined) {
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'admin' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
             console.log('switching to admin mode')
             req.session.user = {}
             req.session.user = { username: 'theadmin', displayName: 'theadmin' }
-            // return res.redirect(req.originalUrl);
-        } else if (req.query.role === 'moderator' && req.session.user === undefined) {
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'moderator' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
             console.log('switching to moderator mode')
             req.session.user = {}
             req.session.user = { username: 'themoderator', displayName: 'themoderator' }
-            // return res.redirect(req.originalUrl);
-        } else if (req.query.role === 'guest' && req.session.user === undefined) {
-            console.log('switching to guest mode')
-            req.session.user = {}
-            req.session.user = { username: 'Earthling', displayName: 'Earthling' }
-            // return res.redirect(req.originalUrl);
-        } else if (req.query.role === 'default' && req.session.user === undefined) {
+            return res.redirect(req.originalUrl);
+        } else if (req.query.role === 'default' && (req.session.user === undefined || roles.includes(req.session.user.username))) {
             console.log('switching to default mode')
             req.session.user = {}
-            req.session.user = { username: 'Alien-life-form', displayName: 'Alien-life-form' }
-            // return res.redirect(req.originalUrl);
+            req.session.user = { username: 'Earthling', displayName: 'Earthling' }
+            return res.redirect(req.originalUrl);
         }
         console.log(`Role query parameter found: ${queryParameter}`)
         // console.log(res.locals.message);
     }
-
-    // Continue to the next middleware if not redirecting
     next();
 });
  
