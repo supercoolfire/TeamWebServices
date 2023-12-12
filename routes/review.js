3d54ce51ba4063b764c59f5f7d589e6de6e6f7e5
supercoolfire <u285916669@byui.edu>
2023-12-13 03:33:14 +0800

    Authentic

--- routes/review.js
+++ routes/review.js
@@ -5,10 +5,10 @@ const reviewsController = require('../controllers/review');
 const validateReview = require('../middleware/validate-reviews');
 const { isAuthenticated, isGod, isAdmin, isModerator } = require('../middleware/authenticate');
 
-router.get('/', isAuthenticated, reviewsController.getAllReviews);
-router.get('/:id', isModerator, reviewsController.getSingleReview);
-router.post('/', isAdmin,validateReview, reviewsController.createReview);
-router.put('/:id', isGod, validateReview, reviewsController.updateReview);
-router.delete('/:id', reviewsController.deleteReview);
+router.get('/', reviewsController.getAllReviews);
+router.get('/:id', isAuthenticated, reviewsController.getSingleReview);
+router.post('/', isAuthenticated, validateReview, reviewsController.createReview);
+router.put('/:id', isAuthenticated, validateReview, reviewsController.updateReview);
+router.delete('/:id', isAuthenticated, reviewsController.deleteReview);
 
 module.exports = router;
