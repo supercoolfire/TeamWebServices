6e579f051589b5c9a47eb6cefbad6b5d371f506c
EMERY RODZIEWICZ <rod20012@byui.edu>
2023-12-11 19:07:12 -0500

    changed host

--- swagger.js
+++ swagger.js
@@ -2,9 +2,9 @@ const swaggerAutogen = require('swagger-autogen');
 
 
 
-const host = 'localhost:3000';
+// const host = 'localhost:3000';
 // const host = 'localhost:3001';
-// const host = 'https://project2-cqkb.onrender.com';
+const host = 'https://project2-cqkb.onrender.com';
 
 const doc = {
     info: {
