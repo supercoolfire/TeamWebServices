052236c5ae00cec0275f1c04278553f192242042
supercoolfire <u285916669@byui.edu>
2023-12-14 08:33:36 +0800

    access level algorithm improved

--- middleware/authenticate-accessLevel.js
+++ middleware/authenticate-accessLevel.js
@@ -7,7 +7,7 @@
  * database:
  {
   "login": "myusername",
-  "role": "god",
+  "accessLevel": 4
  }
  * if use are using userName instead of login, replace
  mongodb.getDatabase().db().findOne({ login: username });
@@ -22,6 +22,18 @@ router.get('/', isAuthenticated, usersController.getAllUsers);
  */
 const mongodb = require('../data/database'); // Update the path as needed
 
+async function getUserAccessLevel(username) {
+  try {
+    const rolesCollection = mongodb.getDatabase().db().collection('roles');
+    const userAccessLevelDoc = await rolesCollection.findOne({ login: username });
+
+    return userAccessLevelDoc ? userAccessLevelDoc.accessLevel : null;
+  } catch (error) {
+    console.error('Error fetching user access level from MongoDB:', error);
+    return null;
+  }
+}
+
 function filterAPI(req, res, status, message) {
   const data = {
     message: message,
@@ -30,40 +42,21 @@ function filterAPI(req, res, status, message) {
   res.status(status).render('index', data);
 }
 
-async function getUserRole(username) {
-  try {
-    const rolesCollection = mongodb.getDatabase().db().collection('roles');
-    const userRole = await rolesCollection.findOne({ login: username });
-
-    return userRole ? userRole.role : null;
-  } catch (error) {
-    console.error('Error fetching user role from MongoDB:', error);
-    return null;
-  }
-}
-
-function authenticateAccessLevel(requiredLevel) {
+function generateAccessLevelMiddlewareInteger(requiredLevel) {
   return async (req, res, next) => {
     const user = req.session.user;
 
     if (!user || !user.username) {
       filterAPI(req, res, 401, 'Unauthorized: You need to login.');
     } else {
-      const userRoles = {
-        god: 4,
-        admin: 3,
-        moderator: 2,
-        authenticated: 1,
-      };
+      // Fetch user's access level from MongoDB
+      const userAccessLevel = await getUserAccessLevel(user.username);
 
-      // Fetch user's role from MongoDB
-      const userRole = await getUserRole(user.username);
+      console.log(`User Access Level: ${userAccessLevel}, Required Level: ${requiredLevel}`);
 
-      console.log(`User Role: ${userRole}, Required Role: ${requiredLevel}`);
-
-      if (userRole && typeof userRoles[requiredLevel] === 'number') {
-        // Check if the user's role level is equal or higher than the required level
-        if (userRoles[userRole] >= userRoles[requiredLevel]) {
+      if (userAccessLevel !== null && typeof userAccessLevel === 'number') {
+        // Check if the user's access level is equal or higher than the required level
+        if (userAccessLevel >= requiredLevel) {
           // User has the required access level, proceed to the next middleware or route
           next();
         } else {
@@ -71,17 +64,14 @@ function authenticateAccessLevel(requiredLevel) {
           filterAPI(req, res, 403, 'Forbidden: Insufficient access level.');
         }
       } else {
-        // Unable to fetch user role or invalid required level
+        // Unable to fetch user access level or invalid required level
         filterAPI(req, res, 500, 'Internal server error.');
       }
     }
   };
 }
 
-// Exporting the middleware and access level constants as an object
+// Exporting the access level middleware function with integer requiredLevel
 module.exports = {
-  isAuthenticated: authenticateAccessLevel('authenticated'),
-  isGod: authenticateAccessLevel('god'),
-  isAdmin: authenticateAccessLevel('admin'),
-  isModerator: authenticateAccessLevel('moderator'),
+  accessLevel: generateAccessLevelMiddlewareInteger,
 };
