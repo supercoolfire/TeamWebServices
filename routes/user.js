a537dd3d1b88e0c4b6e6efde9146ddfa95954589
supercoolfire <u285916669@byui.edu>
2023-12-25 13:24:38 +0800

    included alternative authentication

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const usersController = require('../controllers/user');
 const { validateUser } = require('../middleware/validate-user');
+const { accessLevel } = require('../middleware/authenticate-accessLevel');
 const { isAuthenticated, isGod, isAdmin, isModerator } = require('../middleware/authenticate');
 
 router.get('/', isAuthenticated, usersController.getAllUsers);
 router.get('/:id', isAuthenticated, usersController.getSingleUser);
 
-router.post('/', isAuthenticated, validateUser, usersController.createUser);
+router.post('/', accessLevel(3), validateUser, usersController.createUser);
 router.put('/:id', isAuthenticated, validateUser, usersController.updateUser);
 
-router.delete('/:id', isAuthenticated, usersController.deleteUser);
+router.delete('/:id', isAdmin, usersController.deleteUser);
 
 
 module.exports = router;
